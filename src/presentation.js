126d4fe74200f277c4fbbc1a88004ad02d0c4dc9
thedocbwarren <jedi.master@augmentedjs.com>
2017-12-30 10:37:22 -0800

    adding history and router

--- src/presentation.js
+++ src/presentation.js
@@ -1,13 +1,14 @@
 import AbstractView from "./view/abstractView.js";
 import View from "./view/view.js";
-
+import History from "./history.js";
+import Router from "./router.js";
 /**
  * Augmented.js Next Presentation - The Presentation Component
  *
  * @author Bob Warren
  *
  * @module Presentation
- * @version 2.0.0-alpha.3
+ * @version 2.0.0-alpha.4
  * @license Apache-2.0
  */
 const Presentation = {};
@@ -16,9 +17,11 @@ const Presentation = {};
  * The standard version property
  * @constant VERSION
  */
-Presentation.VERSION = "2.0.0-alpha.3";
+Presentation.VERSION = "2.0.0-alpha.4";
 
 Presentation.AbstractView = AbstractView;
 Presentation.View = View;
+Presentation.History = History;
+Presentation.Router = Router;
 
 export default Presentation;
--- src/router.js
+++ src/router.js
@@ -1,26 +1,50 @@
+import Augmented from "augmentedjs-next";
+import History from "./history.js";
+
+// Cached regular expressions for matching named param parts and splatted
+// parts of route strings.
+const optionalParam = /\((.*?)\)/g;
+const namedParam    = /(\(\?)?:\w+/g;
+const splatParam    = /\*\w+/g;
+const escapeRegExp  = /[\-{}\[\]+?.,\\\^$|#\s]/g;
+
+
+const history = new History();
+
 /**
-* @class Router
-* @extends Backbone.Router
-* @memberof Augmented
-*/
-Augmented.Router = Backbone.Router.extend({
+ * @class Router
+ * Routers map faux-URLs to actions, and fire events when routes are
+ * matched. Creating a new one sets its `routes` hash, if not set statically.
+ * @memberof Presentation
+ */
+class Router {
+  constructor(options) {
+    options || (options = {});
+    if (options.routes) {
+      this.routes = options.routes;
+    }
+    this._bindRoutes();
+    this.initialize(options);
+  };
+
   /**
-  * Load a view safely and remove the last view by calling cleanup, then remove
-  * @method loadView
-  * @param {Augmented.View} view The View to load
-  * @memberof Augmented.Router
-  */
-  loadView: function(view) {
+   * Load a view safely and remove the last view by calling cleanup, then remove
+   * @method loadView
+   * @param {Augmented.View} view The View to load
+   * @memberof Presentation.Router
+   */
+  loadView(view) {
     this.cleanup();
     this._view = view;
     this._view.render();
-  },
+  };
+
   /**
-  * remove the last view by calling cleanup, then remove
-  * @method cleanup
-  * @memberof Augmented.Router
-  */
-  cleanup: function() {
+   * remove the last view by calling cleanup, then remove
+   * @method cleanup
+   * @memberof Presentation.Router
+   */
+  cleanup() {
     if (this._view) {
       if (this._view.cleanup) {
         this._view.cleanup();
@@ -28,5 +52,96 @@ Augmented.Router = Backbone.Router.extend({
       this._view.remove();
       this._view = null;
     }
-  }
-});
+  };
+
+  // Initialize is an empty function by default. Override it with your own
+  // initialization logic.
+  initialize(options) {
+  };
+
+  // Manually bind a single named route to a callback. For example:
+  //
+  //     this.route('search/:query/p:num', 'search', function(query, num) {
+  //       ...
+  //     });
+  //
+  route(route, name, callback) {
+    if (!_.isRegExp(route)) {
+      route = this._routeToRegExp(route);
+    }
+    if (Augmented.isFunction(name)) {
+      callback = name;
+      name = '';
+    }
+    if (!callback) {
+      callback = this[name];
+    }
+    let router = this;
+
+    history.route(route, (fragment) => {
+      const args = router._extractParameters(route, fragment);
+      if (router.execute(callback, args, name) !== false) {
+        router.trigger.apply(router, ['route:' + name].concat(args));
+        router.trigger('route', name, args);
+        history.trigger('route', router, name, args);
+      }
+    });
+    return this;
+  };
+
+  // Execute a route handler with the provided parameters.  This is an
+  // excellent place to do pre-route setup or post-route cleanup.
+  execute(callback, args, name) {
+    if (callback) {
+      callback(args);
+    }
+  };
+
+  // Simple proxy to `history` to save a fragment into the history.
+  navigate(fragment, options) {
+    history.navigate(fragment, options);
+    return this;
+  };
+
+  // Bind all defined routes to `history`. We have to reverse the
+  // order of the routes here to support behavior where the most general
+  // routes can be defined at the bottom of the route map.
+  _bindRoutes() {
+    if (!this.routes) {
+      return;
+    }
+    this.routes = Augmented.result(this, 'routes');
+    let route, routes = Augmented.keys(this.routes);
+    while ((route = routes.pop()) != null) {
+      this.route(route, this.routes[route]);
+    }
+  };
+
+  // Convert a route string into a regular expression, suitable for matching
+  // against the current location hash.
+  _routeToRegExp(route) {
+    route = route.replace(escapeRegExp, '\\$&')
+                 .replace(optionalParam, '(?:$1)?')
+                 .replace(namedParam, (match, optional) => {
+                   return optional ? match : '([^/?]+)';
+                 })
+                 .replace(splatParam, '([^?]*?)');
+    return new RegExp('^' + route + '(?:\\?([\\s\\S]*))?$');
+  };
+
+  // Given a route, and a URL fragment that it matches, return the array of
+  // extracted decoded parameters. Empty or unmatched parameters will be
+  // treated as `null` to normalize cross-browser behavior.
+  _extractParameters(route, fragment) {
+    let params = route.exec(fragment).slice(1);
+    return _.map(params, (param, i) => {
+      // Don't decode the search params.
+      if (i === params.length - 1) {
+        return param || null;
+      }
+      return param ? decodeURIComponent(param) : null;
+    });
+  };
+};
+
+export default Router;
