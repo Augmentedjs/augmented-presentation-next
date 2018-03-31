5513b75e7f169afd9faaa4553970cbc1a6ab8884
thedocbwarren <jedi.master@augmentedjs.com>
2018-03-31 16:24:22 -0700

    adding title and subtitle to header

--- src/view/component/header/header.js
+++ src/view/component/header/header.js
@@ -1,4 +1,5 @@
 import DecoratorView from "../../decorator/decorator.js";
+import Dom from "../../../dom/dom.js";
 
 /**
  * A Header Component
@@ -49,22 +50,7 @@ class Header extends DecoratorView {
     * @returns {object} Returns the view context ('this')
     */
    render() {
-     if (!this.isInitalized) {
-       //_logger.warn("AUGMENTED: Header Can't render yet, not initialized!");
-       return this;
-     }
-     this.template = null;//"notused";
-     if (this.el) {
-       const e = Dom.selector(this.el);
-       if (e) {
-         // the header
-         e.setAttribute("data-" + this.name, "header");
-         e.innerHTML = `${this.title} ${this.subTitle}`;
-       }
-     } else {
-       //_logger.warn("AUGMENTED: Toolbar no element anchor, not rendering.");
-       return this;
-     }
+     Dom.setValue(this.el, `${this.template} ${this.title} ${this.subTitle}`);
      this.delegateEvents();
      this.syncAllBoundElements();
      return this;
